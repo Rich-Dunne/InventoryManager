a28262fbca332d8aa9c51598e9f28c4770ebee5d
Rich Dunne <richdunne@yahoo.com>
2021-08-22 15:58:39 -0600

    Restructuring layout to more closely match AddProduct view.  Added Delete Product button

--- Views/HomeView.xaml.cs
+++ Views/HomeView.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace InventoryManager.Views
 {
@@ -16,12 +10,6 @@ namespace InventoryManager.Views
         public HomeView()
         {
             InitializeComponent();
-        }
-
-        private void btnClose_Click(object sender, RoutedEventArgs e)
-        {
-            //Window parentWindow = Window.GetWindow(this);
-            //parentWindow.Close();
         }
     }
 }
