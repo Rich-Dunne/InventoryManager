00f93db840223dfac1516035cc1aed5118ee2db6
Rich Dunne <richdunne@yahoo.com>
2021-08-22 15:55:24 -0600

    Added DeleteProduct command

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -8,13 +8,16 @@ using System.Windows.Input;
 
 namespace InventoryManager.ViewModels
 {
-    class HomeViewModel : BaseViewModel
+    public class HomeViewModel : BaseViewModel
     {
+        #region Commands
         public ICommand NavigateAddProductCommand { get; }
         public ICommand NavigateAddPartCommand { get; }
+        public ICommand DeleteProductCommand { get; }
+        public RelayCommand<Window> CloseWindowCommand { get; private set; }
+        #endregion
 
         private BaseViewModel _selectedViewModel;
-
         public BaseViewModel SelectedViewModel
         {
             get { return _selectedViewModel; }
@@ -25,14 +28,27 @@ namespace InventoryManager.ViewModels
             }
         }
 
-        public BindingList<Product> Products { get => Inventory.Products; set => OnPropertyChanged("Products"); }
-        public BindingList<Part> Parts { get => Inventory.Parts; set => OnPropertyChanged("Parts"); }
-        public RelayCommand<Window> CloseWindowCommand { get; private set; }
+        public BindingList<Product> Products { get => Inventory.Products; }
+        public BindingList<Part> Parts { get => Inventory.Parts; }
+
+        private object _selectedItem = null;
+        public object SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged("SelectedItem");
+            }
+        }
+
+        // TODO:  Make Product search bar functional
 
         public HomeViewModel(NavigationStore navigationStore)
         {
             NavigateAddProductCommand = new NavigateAddProductCommand(navigationStore);
             NavigateAddPartCommand = new NavigateAddPartCommand(navigationStore);
+            DeleteProductCommand = new DeleteProductCommand(this);
             CloseWindowCommand = new RelayCommand<Window>(CloseWindow);
         }
 
