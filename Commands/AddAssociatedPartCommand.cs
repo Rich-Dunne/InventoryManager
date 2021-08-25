45c7b9ad202670f651677f308fcc2651687a32d6
Rich Dunne <richdunne@yahoo.com>
2021-08-25 06:42:20 -0600

    Fixed parts being added to associated parts datagrid

--- Commands/AddAssociatedPartCommand.cs
+++ Commands/AddAssociatedPartCommand.cs
@@ -1,24 +1,39 @@
 ﻿using InventoryManager.ViewModels;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
 
 namespace InventoryManager.Commands
 {
     public class AddAssociatedPartCommand : CommandBase
     {
-        private AddProductViewModel _viewModel;
+        private AddProductViewModel _addProductViewModel;
+        private ModifyProductViewModel _modifyProductViewModel;
 
         public AddAssociatedPartCommand(AddProductViewModel viewModel)
         {
-            _viewModel = viewModel;
+            _addProductViewModel = viewModel;
+        }
+
+        public AddAssociatedPartCommand(ModifyProductViewModel viewModel)
+        {
+            _modifyProductViewModel = viewModel;
         }
 
         public override void Execute(object part)
         {
-            if(_viewModel.SelectedItem == null || _viewModel.AssociatedParts.Contains((Part)_viewModel.SelectedItem))
+            if (_addProductViewModel?.SelectedItem != null && !_addProductViewModel.AssociatedParts.Contains((Part)_addProductViewModel.SelectedItem))
             {
+                _addProductViewModel.AssociatedParts.Add((Part)_addProductViewModel.SelectedItem);
                 return;
             }
 
-            _viewModel.AssociatedParts.Add((Part)_viewModel.SelectedItem);
+            Part selectedPart = (Part)_modifyProductViewModel.SelectedItem;
+            if (_modifyProductViewModel?.SelectedItem != null && !_modifyProductViewModel.TempAssociatedParts.Any(x => x.PartID == selectedPart.PartID))
+            {
+                _modifyProductViewModel.TempAssociatedParts.Add((Part)_modifyProductViewModel.SelectedItem);
+                return;
+            }
         }
     }
 }
