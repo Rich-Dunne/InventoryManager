accfaef2588f0327cb98c145d0fc579acf057013
Rich Dunne <richdunne@yahoo.com>
2021-08-25 06:44:40 -0600

    Added Parts property for datagrid

--- ViewModels/AddPartViewModel.cs
+++ ViewModels/AddPartViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System.ComponentModel;
+using System.Windows.Input;
 using InventoryManager.Commands;
 using InventoryManager.Stores;
 
@@ -6,7 +7,13 @@ namespace InventoryManager.ViewModels
 {
     public class AddPartViewModel : BaseViewModel
     {
+        #region Commands
         public ICommand NavigateHomeCommand { get; }
+        #endregion
+
+        #region Properties
+        public BindingList<Part> Parts { get; set; } = Inventory.Parts;
+        #endregion
 
         public AddPartViewModel(NavigationStore navigationStore)
         {
