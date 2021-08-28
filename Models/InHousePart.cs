7411db762dc999d3b4a2dd84354c25e57c367518
Rich Dunne <richdunne@yahoo.com>
2021-08-28 11:45:27 -0600

    Added constructors with ID param

--- Models/InHousePart.cs
+++ Models/InHousePart.cs
@@ -13,5 +13,16 @@
             Max = max;
             MachineID = machineID;
         }
+
+        internal InHousePart(int partID, string name, double price, int inventory, int min, int max, int machineID)
+        {
+            PartID = partID;
+            Name = name;
+            Price = price;
+            Inventory = inventory;
+            Min = min;
+            Max = max;
+            MachineID = machineID;
+        }
     }
 }
--- Models/OutsourcedPart.cs
+++ Models/OutsourcedPart.cs
@@ -13,5 +13,16 @@
             Max = max;
             CompanyName = companyName;
         }
+
+        internal OutsourcedPart(int partID, string name, double price, int inventory, int min, int max, string companyName)
+        {
+            PartID = partID;
+            Name = name;
+            Price = price;
+            Inventory = inventory;
+            Min = min;
+            Max = max;
+            CompanyName = companyName;
+        }
     }
 }
