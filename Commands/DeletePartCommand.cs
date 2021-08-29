e341dff2b5ff572ed1d9396a6b47a1b1eeca19dc
Rich Dunne <richdunne@yahoo.com>
2021-08-29 13:02:43 -0600

    Fixed part deletion logic

--- Commands/DeletePartCommand.cs
+++ Commands/DeletePartCommand.cs
@@ -1,4 +1,6 @@
-﻿using InventoryManager.ViewModels;
+﻿using InventoryManager.Models;
+using InventoryManager.ViewModels;
+using System.Linq;
 using System.Windows;
 
 namespace InventoryManager.Commands
@@ -18,12 +20,33 @@ namespace InventoryManager.Commands
                 return;
             }
 
-            var partToRemove = (Part)_viewModel.SelectedPart;
-          
-            var prompt = MessageBox.Show($"Are you sure you want to delete \"{partToRemove.Name}\"?", "Confirm part deletion", MessageBoxButton.YesNo, MessageBoxImage.Warning);
-            if (prompt == MessageBoxResult.Yes)
+            var partToRemove = _viewModel.SelectedPart;
+            
+
+            var warningPrompt = MessageBox.Show($"Are you sure you want to delete \"{partToRemove.Name}\"?", "Confirm part deletion", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (warningPrompt == MessageBoxResult.Yes)
             {
+                bool partBelongsToAnyProduct = Inventory.Products.Any(x => x.AssociatedParts.Any(y => y.PartID == partToRemove.PartID));
+                if (partBelongsToAnyProduct)
+                {
+                    DeletePartFromAllProducts(partToRemove);
+                }
                 Inventory.DeletePart(partToRemove);
+            }
+        }
+
+        private void DeletePartFromAllProducts(Part partToRemove)
+        {
+            var associatedPartPrompt = MessageBox.Show($"Part \"{partToRemove.Name}\" is associated with one or more products.  Would you like to remove this part from all products?", $"Remove part from all products", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (associatedPartPrompt == MessageBoxResult.Yes)
+            {
+                foreach (Product product in Inventory.Products)
+                {
+                    if (product.AssociatedParts.Any(x => x.PartID == partToRemove.PartID))
+                    {
+                        product.RemoveAssociatedPart(partToRemove.PartID);
+                    }
+                }
             }
         }
     }
