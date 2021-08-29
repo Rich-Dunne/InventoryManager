84af119faad74161b5f290854ba82b769119ee6d
Rich Dunne <richdunne@yahoo.com>
2021-08-29 13:03:33 -0600

    Added additional logic for handling products with associated parts

--- Commands/DeleteProductCommand.cs
+++ Commands/DeleteProductCommand.cs
@@ -19,17 +19,36 @@ namespace InventoryManager.Commands
                 return;
             }
 
-            var productToRemove = (Product)_viewModel.SelectedProduct;
+            var productToRemove = _viewModel.SelectedProduct;
             if(productToRemove.AssociatedParts?.Count > 0)
             {
                 MessageBox.Show($"Associated parts must be removed from \"{productToRemove.Name}\" before it can be deleted.");
+                DeleteAssociatedParts(productToRemove);
                 return;
             }
-          
+
+            DeleteProduct(productToRemove);
+        }
+
+        public void DeleteProduct(Product productToRemove)
+        {
             var prompt = MessageBox.Show($"Are you sure you want to delete \"{productToRemove.Name}\"?", "Confirm product deletion", MessageBoxButton.YesNo, MessageBoxImage.Warning);
             if (prompt == MessageBoxResult.Yes)
             {
                 Inventory.RemoveProduct(productToRemove.ProductID);
+            }
+        }
+
+        public void DeleteAssociatedParts(Product productToRemove)
+        {
+            var associatedPartPrompt = MessageBox.Show($"Would you like to remove all associated parts from this product?", $"Remove associated parts", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (associatedPartPrompt == MessageBoxResult.Yes)
+            {
+                foreach (Part part in Inventory.Parts)
+                {
+                    productToRemove.RemoveAssociatedPart(part.PartID);
+                }
+                DeleteProduct(productToRemove);
             }
         }
     }
